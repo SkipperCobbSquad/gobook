5f9327d238ab510cb116831b4340a409000d63ea
Skipper <szymonfaligowski@gmail.com>
2022-07-28 23:04:19 +0200

    In JSON and filed tags

---
+++ ch4/treesort/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type tree struct {
+	value       int
+	left, right *tree
+}
+
+func Sort(values []int) {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	appendValues(values[:0], root)
+}
+
+func appendValues(values []int, t *tree) []int {
+	if t != nil {
+		values = appendValues(values, t.left)
+		values = append(values, t.value)
+		values = appendValues(values, t.right)
+	}
+	return values
+}
+
+func add(t *tree, value int) *tree {
+	if t == nil {
+		//&tree{value: value}
+		t = new(tree)
+		t.value = value
+		return t
+	}
+	if value < t.value {
+		t.left = add(t.left, value)
+	} else {
+		t.right = add(t.right, value)
+	}
+	return t
+}
+func main() {
+	toSort := []int{9, 8, 67, 453, 34, 12, 5, 6, 86789, 23423, 45}
+	Sort(toSort)
+	fmt.Println(toSort)
+}
